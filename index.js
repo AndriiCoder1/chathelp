e9e3914633d69358c35bdcad0eeac511a2ba9f4d
Andrii Kovalov <denisov20051985@gmail.com>
2024-12-03 13:19:45 +0100

    Описание изменений

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ io.on('connection', (socket) => {
 
       // Отправка истории сообщений в OpenAI
       const response = await openai.chat.completions.create({
-        model: 'gpt-4',
+        model: 'gpt-3.5-turbo', 
         messages: userMessages[socket.id],  // Отправка всей истории сообщений для этого пользователя
       });
 
